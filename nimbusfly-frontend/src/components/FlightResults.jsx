4c70058ae1d226eba367a30684a484baeb2dd4c1
Tahmid690 <tahmidhossain690@gmail.com>
2025-06-08 19:53:51 +0600

    flight_search_base

--- nimbusfly-frontend/src/components/FlightResults.jsx
+++ nimbusfly-frontend/src/components/FlightResults.jsx
@@ -13,11 +13,13 @@ function FlightResults() {
             />            
 
             <div className='grid grid-cols-7 gap-4 px-6 lg:px-8'>
-                <div className='col-span-2 bg-red-600 p-4 h-500 ml-40 '>
+                <div className='col-span-2 bg-red-600 p-4 h-500 ml-30'>
+                    {/* Eikhane Filter Thakbe */}
 
                 </div>
 
-                <div className='col-span-5 bg-teal-400 p-4 h-500 mr-40'>
+                <div className='col-span-5 bg-teal-400 p-4 h-500 mr-30'>
+                    {/* Eikhne Flight Details Thakbe */}
 
                 </div>
 
