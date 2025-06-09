223e168b4db5578745d215f565b81f17c551aa95
Tahmid <45348952+Tahmid690@users.noreply.github.com>
2025-06-09 15:54:40 +0600

    Price_query updt

--- controllers/flightsController.js
+++ controllers/flightsController.js
@@ -153,18 +153,64 @@ const searchFlights = async (req, res) => {
     orderType
   } = req.query;
 
-  // console.log("yeppi");
+  // console.log(req.query);
+
 
   try {
 
-    let flights = [];
-
     //One-Way
+    let order_typ;
+    // console.log(orderType);
+    if(orderType==='Cheapest'){
+      if(seatClass==='Business') order_typ=`f.business_ticket_price`;
+      else order_typ=`f.economy_ticket_price`;
+    } 
+    else if(orderType==='Fastest') order_typ=`f.arrival_time - f.departure_time`;
+    else order_typ=`f.departure_time`
 
-    const query1=`SELECT * FROM flights`;
-    const result=await pool.query(query1);
+    
+    let total = 0.75*parseInt(children || 0) + parseInt(adults || 0);
+
+    // console.log(total);
+
+    const query1 = `
+      SELECT
+        a.airline_name as airline_name,
+        f.departure_time as departure_time,
+        f.arrival_time as arrival_time,
+        (f.arrival_time - f.departure_time) as flight_time,
+        (CASE 
+            WHEN $4 = 'Business' THEN f.business_ticket_price 
+            ELSE f.economy_ticket_price 
+        END)*($6) as ticket_price,
+        f.baggage_limit as baggage_limit,
+        ac.model as aircraft_name,
+        f.flight_number as flight_number
+    FROM
+        flights f
+        JOIN aircraft ac ON ac.aircraft_id = f.aircraft_id
+        JOIN airlines a ON ac.airline_id = a.airline_id
+        JOIN airports apo ON apo.airport_id = f.origin_airport_id
+        JOIN airports apd ON apd.airport_id = f.destination_airport_id
+        WHERE 
+            apo.iata_code = $1 AND
+            apd.iata_code = $2 AND
+            DATE(f.departure_time) = $3 AND
+            (
+                CASE 
+                    WHEN $4 = 'Business' THEN f.available_busi_seats 
+                    ELSE f.available_econ_seats 
+                END >= $5
+            ) 
+        ORDER BY `+order_typ;  
+    // console.log(query1);
+    const result = await pool.query(query1,[origin,destination,journeyDate,seatClass,parseInt(adults || 0)+parseInt(children || 0),total]);
+   
+    // const query1=`SELECT * FROM flights`;
+    // const result = await pool.query(query1);
+    // console.log(result.rows);
     return res.json({
-      data:result.rows
+      data: result.rows
     });
 
   }
