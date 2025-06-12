58f281b84e7cf60d63c06a4f93a27fbedec36469
Tahmid690 <tahmidhossain690@gmail.com>
2025-06-12 06:00:22 +0600

    booking

--- nimbusfly-frontend/src/components/BookingDetails.jsx
+++ nimbusfly-frontend/src/components/BookingDetails.jsx
@@ -1,18 +1,21 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
 import BookingStages from "./BookingStages";
 
 
-const BookingDetails = ()=>{
+const BookingDetails = () => {
     const location = useLocation();
     const flight = location.state;
     console.log(flight);
 
-    return(
+    return (
         <>
-            <Navbar flg={true}/>
-            <BookingStages stage={1}/>
+            <Navbar flg={true} />
+            <div className="mt-20">
+                <BookingStages stage={1}/>
+            </div>
+
         </>
     );
 
