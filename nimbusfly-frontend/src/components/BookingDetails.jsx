d9886a30388fe0115e3e39c212daa68555722616
Tanvir Hossen Zihad <tanvirzihad1988@gmail.com>
2025-06-13 15:48:57 +0600

    bookingpagevaag

--- nimbusfly-frontend/src/components/BookingDetails.jsx
+++ nimbusfly-frontend/src/components/BookingDetails.jsx
@@ -16,6 +16,19 @@ const BookingDetails = () => {
             <div className="mt-20">
                 <BookingStages stage={1}/>
             </div>
+            <div className="flex px-6 lg:px-8 mt-10 h-screen">
+                <div className="w-3/4 flex-shrink-0 overflow-y-auto p-4 ml-30">
+                  {/* flight details and passenger form */}
+
+                </div>
+
+                <div className="w-1/4 flex-shrink-0 overflow-y-auto p-4 mr-30">
+                    {/* ticket summary */}
+
+                </div>
+
+
+            </div>
 
         </>
     );
